bc087f37f4f2e137b3b8101ce9711f3f2bfc4423
singhk <CORP\singhk>
2020-01-05 06:42:27 +0000

    change in code
    git-tfs-id: [http://desktop-rd5g1bc/test]$/SAPProject;C36

--- Dev/Saptest/Saptest/Program.cs
+++ Dev/Saptest/Saptest/Program.cs
@@ -14,6 +14,7 @@ namespace Saptest
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            print("test message")
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
